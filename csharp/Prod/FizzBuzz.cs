5a75cfa3001fdb7bdd8d839cb8de4a3f81e1b5af
Romain Deneau <rdeneau@yahoo.fr>
2017-12-28 15:22:56 +0100

    FizzBuzz complet, avec un simili pattern matching F#

--- csharp/Prod/FizzBuzz.cs
+++ csharp/Prod/FizzBuzz.cs
@@ -1,10 +1,37 @@
-﻿namespace Kata.Prod
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.Prod
 {
     public static class FizzBuzz
     {
-        public static string Compute(int number)
+        public static string Generate(int number)
         {
-            return $"{number}";
+            switch (IsDivisibleBy(number, 3, 5))
+            {
+                case "3,_": return "Fizz";
+                case "_,5": return "Buzz";
+                case "3,5": return "FizzBuzz";
+                default : return $"{number}";
+            }
+        }
+
+        private static string IsDivisibleBy(int number, params int[] dividors)
+        {
+            return dividors.Select(dividor => IsDivisibleBySingle(number, dividor))
+                           .Join(",");
+        }
+
+        private static string IsDivisibleBySingle(int number, int dividor)
+        {
+            return number % dividor == 0
+                ? $"{dividor}"
+                : "_";
+        }
+
+        private static string Join<T>(this IEnumerable<T> @this, string separator)
+        {
+            return string.Join(separator, @this);
         }
     }
 }
--- csharp/Tests/FizzBuzzShould.cs
+++ csharp/Tests/FizzBuzzShould.cs
@@ -10,11 +10,44 @@ namespace Kata.Tests
         [InlineData(1)]
         [InlineData(2)]
         [InlineData(4)]
-        public void Return_Given_Number_When_Not_Divisible_By_3_And_5(int number)
+        public void Return_The_Given_Number_When_Not_Divisible_By_3_And_5(int number)
         {
-            var result = FizzBuzz.Compute(number);
+            var result = FizzBuzz.Generate(number);
             Check.That(result)
                  .Equals($"{number}");
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        public void Fizz_Given_A_Number_When_Divisible_By_3_But_Not_By_5(int number)
+        {
+            var result = FizzBuzz.Generate(number);
+            Check.That(result)
+                 .Equals("Fizz");
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(20)]
+        public void Buzz_Given_A_Number_When_Divisible_By_5_But_Not_By_3(int number)
+        {
+            var result = FizzBuzz.Generate(number);
+            Check.That(result)
+                 .Equals("Buzz");
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        [InlineData(45)]
+        public void Buzz_Given_A_Number_When_Divisible_By_3_And_By_5(int number)
+        {
+            var result = FizzBuzz.Generate(number);
+            Check.That(result)
+                 .Equals("FizzBuzz");
         }
     }
 }
