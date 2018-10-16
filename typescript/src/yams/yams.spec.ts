8ab30ed1ad5cf183746c360f089a4531ec7fe15e
CaptpBdcht <vincent_mot@hotmail.fr>
2018-10-16 11:41:55 +0200

    Refactored code: added updated linting
    Added ThreeOfAKind and FourOfAKind

--- typescript/src/yams/yams.spec.ts
+++ typescript/src/yams/yams.spec.ts
@@ -1,5 +1,5 @@
 // Using Chai BDD/Should style: http://chaijs.com/api/bdd/
-import {should} from 'chai';
+import { should } from 'chai';
 
 import {
     Combinations,
@@ -11,50 +11,90 @@ should();
 describe('YamsCombinationCalculatorShould', () => {
 
     it('return one when doing one one on "Ones"', () => {
-        const roll = [1, 0, 0, 0, 0];
+        const roll = [ 1, 0, 0, 0, 0 ];
         const expected = 1;
 
         YamsCombinationCalculator(roll, Combinations.Ones)
             .should.equal(expected);
-    })
+    });
 
     it('return two when doing two one on "Ones"', () => {
-        const roll = [1, 1, 0, 0, 0];
+        const roll = [ 1, 1, 0, 0, 0 ];
         const expected = 2;
 
         YamsCombinationCalculator(roll, Combinations.Ones)
             .should.equal(expected);
-    })
+    });
 
     it('return five when doing five one on "Ones"', () => {
-        const roll = [1, 1, 1, 1, 1];
+        const roll = [ 1, 1, 1, 1, 1 ];
         const expected = 5;
 
         YamsCombinationCalculator(roll, Combinations.Ones)
             .should.equal(expected);
-    })
+    });
 
     it('return two when doing one two on "Twos"', () => {
-        const roll = [2, 0, 0, 0, 0];
+        const roll = [ 2, 0, 0, 0, 0 ];
         const expected = 2;
 
         YamsCombinationCalculator(roll, Combinations.Twos)
             .should.equal(expected);
-    })
+    });
 
     it('return four when doing two two on "Twos"', () => {
-        const roll = [2, 2, 0, 0, 0];
+        const roll = [ 2, 2, 0, 0, 0 ];
         const expected = 4;
 
         YamsCombinationCalculator(roll, Combinations.Twos)
             .should.equal(expected);
-    })
+    });
 
     it('return three when doing one three on "Threes"', () => {
-        const roll = [3, 0, 0, 0, 0];
+        const roll = [ 3, 0, 0, 0, 0 ];
         const expected = 3;
 
         YamsCombinationCalculator(roll, Combinations.Threes)
             .should.equal(expected);
-    })
+    });
+
+    it('return 3 when doing a "ThreeOfAKind" with three ones', () => {
+        const roll = [ 1, 1, 1, 0, 0 ];
+        const expected = 3;
+
+        YamsCombinationCalculator(roll, Combinations.ThreeOfAKind)
+            .should.equal(expected);
+    });
+
+    it('return 6 when doing a "ThreeOfAKind" with three twos', () => {
+        const roll = [ 2, 2, 2, 0, 0 ];
+        const expected = 6;
+
+        YamsCombinationCalculator(roll, Combinations.ThreeOfAKind)
+            .should.equal(expected);
+    });
+
+    it('return 6 when doing a "ThreeOfAKind" with more than three twos', () => {
+        const roll = [ 2, 2, 2, 2, 2 ];
+        const expected = 6;
+
+        YamsCombinationCalculator(roll, Combinations.ThreeOfAKind)
+            .should.equal(expected);
+    });
+
+    it('return 4 when doing a "FourOfAKind" with four ones', () => {
+        const roll = [ 1, 1, 1, 1, 0 ];
+        const expected = 4;
+
+        YamsCombinationCalculator(roll, Combinations.FourOfAKind)
+            .should.equal(expected);
+    });
+
+    it('return 4 when doing a "FourOfAKind" with more than four ones', () => {
+        const roll = [ 1, 1, 1, 1, 1 ];
+        const expected = 4;
+
+        YamsCombinationCalculator(roll, Combinations.FourOfAKind)
+            .should.equal(expected);
+    });
 });
--- typescript/src/yams/yams.ts
+++ typescript/src/yams/yams.ts
@@ -31,10 +31,36 @@ export class ThreesCombination implements Combination {
     }
 }
 
+export class ThreeOfAKindCombination implements Combination {
+    getResultFor(roll: number[]): number {
+        const dicesOccurences = getDicesOccurences(roll);
+        const kind = dicesOccurences.findIndex(dice => dice >= 3) + 1;
+
+        return kind * 3;
+    }
+}
+
+class FourOfAKindCombination implements Combination {
+    getResultFor(roll: number[]): number {
+        const dicesOccurences = getDicesOccurences(roll);
+        const kind = dicesOccurences.findIndex(dice => dice >= 4) + 1;
+
+        return kind * 4;
+    }
+}
+
+function getDicesOccurences(roll: number[]): number[] {
+    const occurences = [ 0, 0, 0, 0, 0, 0 ];
+    roll.forEach(dice => occurences[dice - 1]++);
+    return occurences;
+}
+
 export class Combinations {
     static Ones: Combination = new OnesCombination();
     static Twos: Combination = new TwosCombination();
     static Threes: Combination = new ThreesCombination();
+    static ThreeOfAKind: Combination = new ThreeOfAKindCombination();
+    static FourOfAKind: Combination = new FourOfAKindCombination();
 }
 
 export function YamsCombinationCalculator(roll: number[], combination: Combination): number {
